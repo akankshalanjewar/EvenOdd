d8f7a24eae638a0e86169f378b8fc5650c94cafe
akankshalanjewar <akankshalanjewar02@gmail.com>
2022-03-12 17:10:35 +0530

    added[Akanksha]EvenOdd

---
+++ EvenOdd/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EvenOdd
+{
+     public class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter The Number");
+            num=int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < num/2; i++)
+            {
+                if(num%2==0)
+                {
+                    Console.WriteLine("Even");
+                }
+                else
+                {
+                    Console.WriteLine("Odd");
+                }
+            }
+        }
+    }
+}
